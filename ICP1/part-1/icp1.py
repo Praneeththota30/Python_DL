38287b4ed94ce56025c6b5e40d776366892a0034
Praneeth <praneethvuf18015@gmail.com>
2019-08-23 19:36:38 -0500

    icp1
    code

---
+++ ICP1/part-1/icp1.py
@@ -0,0 +1,10 @@
+str1 = input('enter the string: ')
+print(str1)
+
+#str2 = str1.replace("p", "")
+#str3 = str2.replace("y", "")
+str2 = str1[2:]
+print(str2)
+print(str2[::-1])
+
+#print('python')
---
+++ ICP1/part-1/task2.py
@@ -0,0 +1,5 @@
+
+str1 = " love playing with python"
+str2 = str1.replace("python", "pythons")
+print(str1)
+print(str2)
