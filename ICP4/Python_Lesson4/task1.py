edc258505f9a8eceaff210d070411c5f62aeef60
Praneeth <praneethvuf18015@gmail.com>
2019-09-13 18:09:59 -0500

    ICP4
    ICP4

---
+++ ICP4/Python_Lesson4/task1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+train_df = pd.read_csv('./train.csv')
+
+X_train = train_df.drop('Survived',axis=1)
+Y_train = train_df["Survived"]
+
+train_df['Sex'] =train_df['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+print(train_df['Sex'].corr(train_df['Survived']))
